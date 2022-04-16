4fd74ec0efd183053802e2e8bf14bf1671e531d6
Joel Chantrell <joelchantrell@gmail.com>
2022-04-16 14:02:37 +0930

    exercise 5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
@@ -41,4 +41,30 @@ test('blog displays url and likes when show button is clicked', async () => {
 
     const div = document.querySelector('.detailsShown')
     expect(div).toHaveStyle('display: block')
+})
+
+test('if like button is clicked twice, the event handler is called twice', async () => {
+    const blog = {
+        title: 'a title',
+        author: 'an author',
+        url: 'the url',
+        user: { username: 'a user' }
+    }
+
+    const user = {
+        username: 'a user'
+    }
+
+    const addLikes = jest.fn()
+
+    render(<Blog blog={blog} user={user} updateLikes={addLikes} />)
+
+    const showButton = document.querySelector('.detailsShowButton')
+    await userEvent.click(showButton)
+
+    const likeButton = screen.getByText('like')
+    await userEvent.click(likeButton)
+    await userEvent.click(likeButton)
+
+    expect(addLikes.mock.calls).toHaveLength(2)
 })
